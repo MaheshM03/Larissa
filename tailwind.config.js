eeddbf438d9851d3f9429c1295db2d4943097db9
MaheshM03 <misalmahesh03@gmail.com>
2025-07-21 18:59:41 +0530

    Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",   // 👈 This tells Tailwind to scan your React code
+    "./public/index.html"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
